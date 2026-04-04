6eb1926082ba9fc9e785648fc9c3eae9a610991d
MaxKot22 <107506360+MaxKot22@users.noreply.github.com>
2026-04-04 07:19:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ TOKEN = os.getenv('TELEGRAM_TOKEN')
 GEMINI_KEY = os.getenv('GEMINI_API_KEY')
 
 genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
